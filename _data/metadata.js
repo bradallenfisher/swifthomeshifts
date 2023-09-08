c912aa754af8d23a69ada437cbe38c38654db4e2
bradallenfisher <bradallenfisher@gmail.com>
2023-09-08 10:53:24 -0400

    first commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Swift Home Shifts",
+	url: "https://swifthomeshifts.com",
+	language: "en",
+	description: "Sell your house swiftly",
+	author: {
+		name: "Grant Garab",
+		email: "grant@wearehomebuyers.com",
+		url: "https://www.wearehomebuyers.com/author/grant-garab/"
+	}
+}
