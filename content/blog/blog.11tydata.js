bb61fd9a208a00113bccf54fef81f78aea762c60
bradallenfisher <bradallenfisher@gmail.com>
2023-11-03 10:43:52 -0400

    new posts

--- content/blog/blog.11tydata.js
+++ content/blog/blog.11tydata.js
@@ -3,4 +3,11 @@ module.exports = {
 		"posts"
 	],
 	"layout": "layouts/post.njk",
+	sitename: "Swift Home Shifts",
+    sitedesc: "Selling your house fast",
+    siteurl: "https://swifthomeshifts.com",
+    siteimgsrc: "/img/logo.png",
+    siteimgwidth: "500",
+    siteimgheight: "500",
+    author: "Katie Rossi"
 };
